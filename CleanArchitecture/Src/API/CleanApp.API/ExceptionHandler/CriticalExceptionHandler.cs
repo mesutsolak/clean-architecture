3223dc08356b86736b088ce52dd13411e44aa0d9
Mesut Solak <mesuttsolakk@gmail.com>
2024-12-17 23:31:14 +0300

    services were added to the api project

---
+++ CleanArchitecture/Src/API/CleanApp.API/ExceptionHandler/CriticalExceptionHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanApp.API.ExceptionHandler;
+
+public class CriticalExceptionHandler : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is CriticalException)
+        {
+            Console.WriteLine("hata ile ilgili sms gönderildi");
+        }
+
+        return ValueTask.FromResult(false);
+    }
+}
--- CleanArchitecture/Src/API/CleanApp.API/Program.cs
+++ CleanArchitecture/Src/API/CleanApp.API/Program.cs
@@ -1,11 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<FluentValidationFilter>();
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddProblemDetails();
+
+builder.Services.AddRepositories(builder.Configuration)
+    .AddServices();
+
+builder.Services.AddScoped(typeof(NotFoundFilter<,>));
+
+builder.Services.AddExceptionHandler<CriticalExceptionHandler>();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 var app = builder.Build();
 
